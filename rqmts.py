c3da7b0d5239e6f699d00b3460d8cc3a135c5218
Piyush Raj <piyush.189303055@muj.manipal.edu>
2019-07-19 03:29:45 +0530

    fixes #3: implemented command-line iface

--- rqmts.py
+++ rqmts.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
-import os, sys
+import os, sys, argparse
 from parsing import check, parse, call
 
-requirements_list = []
-pfreeze_cmd = "pip freeze"
-pshow_cmd = "pip show {}"
+parser = argparse.ArgumentParser(description='rqmts - Generate pip requirements.txt file for any project')
+#optional = parser._action_groups.pop() # popped opt args
+#optional = parser.add_argument_group('Parameters')
+parser.add_argument("-p", "--path", dest="path", default=False, help= "Path of the Python script")
 
-file_path = input("Enter path of the python script : ")
+file_path = parser.parse_args().path
+if file_path == False:
+    print("[*] Path not provided, invoking interactive mode ...")
+    file_path = input("Enter the path of Python script : ")
+
 if os.path.exists(file_path):
  dir_path = os.path.dirname(file_path)
  with open(file_path, 'r') as f:
@@ -21,12 +26,17 @@ else:
  print("[-] Invalid path. Exiting ...")
  sys.exit(1)
 
+requirements_list = []
+pfreeze_cmd = "pip freeze"
+pshow_cmd = "pip show {}"
+REQS_PATH = dir_path + "\\requirements.txt"
 INSTALLED_PKGS = call(pfreeze_cmd).split()
 PARSED_PKG_LIST = parse(code)
+
 for package in PARSED_PKG_LIST:
     pkg_info = call(pshow_cmd.format(package))
-    pkg_query = pkg_info.split("Name: ")[1].split('\nVersion:')[0]
     try:
+        pkg_query = pkg_info.split("Name: ")[1].split('\nVersion:')[0]
         result = check(pkg_query, INSTALLED_PKGS)
         requirements_list.append(result)
     except:
@@ -35,11 +45,10 @@ for package in PARSED_PKG_LIST:
 print("[+] Success: Parsed the dependencies correctly")
 print("[*] Saving generated requirements.txt")
 
-REQS_PATH = dir_path + "\\requirements.txt"
 with open(REQS_PATH, 'w') as g:
     for req in requirements_list:
         g.write(req + '\n')
-    f.close()
+    g.close()
 
 print("[+] Success: requirements.txt saved")
 print("[+] Path where it can be found: {}".format(REQS_PATH))
