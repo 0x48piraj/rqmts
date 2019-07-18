d3966d15f2b42271f1f3242ce0665b926ac93805
Piyush Raj <piyush.189303055@muj.manipal.edu>
2019-07-19 01:03:51 +0530

    fixes #2: high on modular programming

---
+++ parsing.py
@@ -0,0 +1,25 @@
+def query(qry, data):
+ for pkg in data:
+     if qry in pkg:
+         return pkg
+
+def parse_modules(code):
+    modules = []
+    code = code.splitlines()
+    for item in code:
+        if item[:7] == "import " and ", " not in item:
+            if " as " in item:
+                modules.append(item[7:item.find(" as ")])
+            else:
+                modules.append(item[7:])
+        elif item[:5] == "from ":
+            if "." in item[5:item.find(" import ")]:
+                modules.append(item[5:item.find(" import ")].split(".")[0])
+            else:
+                modules.append(item[5:item.find(" import ")])
+        elif ", " in item:
+            item = item[7:].split(", ")
+            modules = modules+item
+        else:
+            print(item)
+    return modules
--- rqmts.py
+++ rqmts.py
@@ -1,36 +1,20 @@
-import os, subprocess
-
-def query(qry, data):
- for pkg in data:
-     if qry in pkg:
-         return pkg
-
-def parse_modules(code):
-    modules = []
-    code = code.splitlines()
-    for item in code:
-        if item[:7] == "import " and ", " not in item:
-            if " as " in item:
-                modules.append(item[7:item.find(" as ")])
-            else:
-                modules.append(item[7:])
-        elif item[:5] == "from ":
-            if "." in item[5:item.find(" import ")]:
-                modules.append(item[5:item.find(" import ")].split(".")[0])
-            else:
-                modules.append(item[5:item.find(" import ")])
-        elif ", " in item:
-            item = item[7:].split(", ")
-            modules = modules+item
-        else:
-            print(item)
-    return modules
+import os, sys, subprocess
+from parsing import query, parse_modules
 
 file_path = input("Enter path of the python script : ")
-dir_path = os.path.dirname(file_path)
-with open(file_path, 'r') as f:
-    code = f.read()
-    f.close()
+
+if os.path.exists(file_path):
+ dir_path = os.path.dirname(file_path)
+ with open(file_path, 'r') as f:
+    try:
+        code = f.read()
+        f.close()
+    except:
+        print("[-] File failed to load. Exiting ...")
+        sys.exit(1)
+else:
+ print("[-] Invalid path. Exiting ...")
+ sys.exit(1)
 
 requirements_list = []
 data = subprocess.getoutput("pip freeze").split()
