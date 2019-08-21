1b7c2208e2e6f7490fd88eb57c6e98a811ef251b
Piyush Raj <piyush.189303055@muj.manipal.edu>
2019-08-21 23:16:19 +0530

    version: bumped the version to 2.0.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(os.path.join(pwd, "README.md")) as f:
 # call to setup()
 setup(
     name="rqmts",
-    version="1.0.0",
+    version="2.0.1",
     description="Generates pip requirements.txt file for any project by analysing package imports",
     long_description=README,
     long_description_content_type="text/markdown",
